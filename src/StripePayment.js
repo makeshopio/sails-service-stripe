57db2258e82a59ef04c87f8db8abd96cbd6e255b
Aaron Dancer <me@aarondancer.com>
2016-11-02 15:25:06 -0500

    Customer, Subscribe, Cancel

--- src/StripePayment.js
+++ src/StripePayment.js
@@ -10,6 +10,73 @@ export default class StripePayment extends BasePayment {
   }
 
   /**
+   * Create a Stripe customer
+   * @param {Object} customer Credit card data and user info
+   * @param {Object} _config Additional configuration
+   * @return {Promise}
+   */
+  
+  createCustomer(customer, _config) {
+    let config = _.merge({
+      email: customer.email,
+      phone: customer.phone,
+      source: {
+        object: 'card',
+        number: customer.cardNumber,
+        exp_month: customer.expMonth,
+        exp_year: customer.expYear,
+        cvc: customer.cvc,
+        name: customer.cardHolderName
+      }
+    }, _config);
+
+    return new Promise((resolve, reject) => {
+      this.getProvider().customers.create(
+        config,
+        (err, res) => err ? reject(err) : res(res)
+      );
+    })
+  }
+
+  /**
+   * Create subscription for customer
+   * @param {Object} subscription Credit card data and user info
+   * @param {Object} _config Additinoal configuration
+   * @returns {Promise}
+   */
+  
+  subscribe(subscription, _config) {
+    let config = _.merge({
+      plan: subscription.plan
+    }, _config);
+
+    return new Promise((resolve, reject) => {
+      this.getProvider().customers.createSubscription(
+        subscription.identifier,
+        config,
+        (err, res) => err ? reject(err) : resolve(res)
+      )
+    })
+  }
+
+ /**
+  * Cancel a customer's subscription
+  * @param {String} customerId
+  * @param {String} subscriptionId
+  * @returns {Promise}
+  */
+ 
+ cancelSubscription(customerId, subscriptionId) {
+  return new Promise((resolve, reject) => {
+    this.getProvider().customers.cancelSubscription(
+      customerId,
+      subscriptionId,
+      (err, res) => err ? reject(err) : resolve(res)
+    )
+  });
+ }
+
+  /**
    * Checkout credit card
    * @param {Object} _creditCard Credit card data
    * @param {Object} [_config] Additional configuration for provider
